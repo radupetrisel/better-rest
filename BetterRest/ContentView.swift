7266f57a3ac424510abe3c35040d22a4e7116f0e
Radu Petrisel <radupetrisel@gmail.com>
2023-07-08 18:21:50 +0300

    Create app based on tutorial
    * add DatePicker for choosing wake up time
    * add Steppers for desired sleep amount and number of coffee cups the user drinks
    * add toolbar item for calculating the ideal bedtime
    * add ML model for predicting the ideal sleep amount

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -5,17 +5,84 @@
 //  Created by Radu Petrisel on 08.07.2023.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
+    private static var defaultWakeUpTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        return Calendar.current.date(from: components) ?? Date.now
+    }
+    
+    @State private var wakeUpTime = Self.defaultWakeUpTime
+    @State private var desiredSleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                    
+                    DatePicker("Please enter a time", selection: $wakeUpTime, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                }
+                
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("How much do you want to sleep?")
+                        .font(.headline)
+                    
+                    Stepper("\(desiredSleepAmount.formatted()) hours", value: $desiredSleepAmount, in: 4...12, step: 0.25)
+                }
+                
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("How much coffee do you drink?")
+                        .font(.headline)
+                    
+                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                }
+            }
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Calculate", action: calculateBedtime)
+            }
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("Ok") {
+                    
+                }
+            } message: {
+                Text(alertMessage)
+            }
         }
-        .padding()
+    }
+    
+    private func calculateBedtime() {
+        do {
+            let config = MLModelConfiguration()
+            let sleepCalculator = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUpTime)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try sleepCalculator.prediction(wake: Double(hour + minute), estimatedSleep: desiredSleepAmount, coffee: Double(coffeeAmount))
+            let estimatedBedtime = wakeUpTime - prediction.actualSleep
+            
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = estimatedBedtime.formatted(date: .omitted, time: .shortened)
+            
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
+        
+        showingAlert = true
     }
 }
 
